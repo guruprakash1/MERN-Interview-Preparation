cc0d303c3dc3e1cb6276e80464d7db27ac5ab321
guruprakash1 <prakash.kumarguru@gmail.com>
2024-01-24 19:16:19 +0530

    Update js-output.js

--- js-output.js
+++ js-output.js
@@ -1,5 +1,20 @@
+//Give the output of below code
 console.log(null==undefined);
 
 //Output
-
 true
+
+//Write a mul function which will return the multipliaction of these 3
+// console.log(mul(2)(3)(4))->24
+
+function mul(a){
+  return function(b) {
+    return function(c) {
+      return a * b * c;
+    }
+  }
+}
+
+console.log(mul(2)(3)(4));
+//Output
+24
