97d2d2faed6b9811471faada6ecb9c3e2ad3897a
guruprakash1 <prakash.kumarguru@gmail.com>
2024-02-22 08:42:35 +0530

    Create async_await_2.js

---
+++ Javascript/Async-Await/async_await_2.js
@@ -0,0 +1,2 @@
+When you use async and await in a function, it indicates that the function contains asynchronous operations. However, it's essential to understand that async and await work together to simplify asynchronous code and make it look more like synchronous code, but they don't alter the fundamental behavior of asynchronous execution.
+
